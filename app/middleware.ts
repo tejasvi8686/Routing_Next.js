70e9b802611c3241adee39113feb465374b6051f
tejasviappeneure <tejasvi@appeneure.com>
2024-09-22 22:01:54 +0530

    middleware.ts

---
+++ app/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+// const isLoggedIn = false;
+
+export function middleware(request) {
+  // console.log("This is middleware");
+  // if (!isLoggedIn && request.nextUrl.pathname !== '/home') {
+  return NextResponse.redirect(new URL("/home", request.url));
+  // }
+}
+
+export const config = {
+  matcher: ["/contact/:path*"], // Added * to match all paths under /contact
+};
